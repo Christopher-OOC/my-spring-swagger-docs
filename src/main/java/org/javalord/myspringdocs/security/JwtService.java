63045aad103f2b6bf74fc9cf0049f2a6638b5663
Christopher-OOC <olojedechristopher24@gmail.com>
2026-03-11 10:20:27 +0100

    shjjyhdkjl

--- src/main/java/org/javalord/myspringdocs/security/JwtService.java
+++ src/main/java/org/javalord/myspringdocs/security/JwtService.java
@@ -24,6 +24,12 @@ public class JwtService {
     @Value("{application.security.jwt.secret}")
     private String secret;
 
+    @Value("{application.security.jwt.access-token-expiration}")
+    private long accessTokenExpiration;
+
+    @Value("{application.security.jwt.refresh-token-expiration}")
+    private long refreshTokenExpiration;
+
     public JwtService() {
         byte[] byteSecret = Base64.getDecoder().decode(secret);
         this.key = Keys.hmacShaKeyFor(byteSecret);
@@ -32,13 +38,39 @@ public class JwtService {
     public AuthResponse generateToken(String email, String password) {
         AuthResponse response = new AuthResponse();
         response.setTokenType("Bearer");
-        response.setAccessToken();
+        response.setAccessToken(generateAccessToken(email));
+        response.setRefreshToken(generateRefreshToken(email));
+
+        return response;
+    }
+
+    public AuthResponse refreshToken(String refreshToken) {
+        String email = extractEmail(refreshToken);
+
+        if (!validateRefreshToken(refreshToken)) {
+            throw new JwtException("Invalid refresh token");
+        }
+
+        AuthResponse response = new AuthResponse();
+        response.setTokenType("Bearer");
+        response.setAccessToken(generateAccessToken(email));
+        response.setRefreshToken(generateRefreshToken(email));
+
+        return response;
+    }
+
+    private boolean validateRefreshToken(String refreshToken) {
+        if (!isJwtValid(refreshToken)) {
+            throw new JwtException("Invalid refresh token");
+        }
+
+        return extractClaims(refreshToken).get("TOKEN_TYPE").equals("REFRESH_TOKEN");
 
     }
 
     private String generateAccessToken(String email) {
         try {
-            return buildToken(email, Map.of("TOKEN_TYPE", "ACCESS_TOKEN"));
+            return buildToken(email, Map.of("TOKEN_TYPE", "ACCESS_TOKEN"), accessTokenExpiration);
         }
         catch (JwtException e) {
             throw new JwtException("Invalid JWT Token");
@@ -47,21 +79,21 @@ public class JwtService {
 
     private String generateRefreshToken(String email) {
         try {
-            return buildToken(email, Map.of("TOKEN_TYPE", "REFRESH_TOKEN"));
+            return buildToken(email, Map.of("TOKEN_TYPE", "REFRESH_TOKEN"), refreshTokenExpiration);
         }
         catch (JwtException e) {
             throw new JwtException("Invalid JWT Token");
         }
     }
 
-    private String buildToken(String email, Map<String, String> claims) {
+    private String buildToken(String email, Map<String, String> claims, long expiration) {
         try {
             return Jwts.builder()
                     .signWith(this.key)
                     .subject(email)
                     .claims(claims)
                     .issuedAt(new Date())
-                    .expiration(new Date(System.currentTimeMillis() + 1200000))
+                    .expiration(new Date(System.currentTimeMillis() + expiration))
                     .compact();
         }
         catch (JwtException e) {
